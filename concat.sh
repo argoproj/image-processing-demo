676486df53c2dd3a6b47aa478dc4d00c29a9851f
Alexander Matyushentsev <alexander@applatix.com>
2017-11-09 17:28:25 -0800

    Initial commit

---
+++ concat.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+OUTPUT0=($(ls -d ./output/0/????.jpg))
+OUTPUT1=($(ls -d ./output/1/????.jpg))
+OUTPUT2=($(ls -d ./output/2/????.jpg))
+OUTPUT3=($(ls -d ./output/3/????.jpg))
+
+mkdir -p ./output/result
+
+for i in {0..4}; do
+    convert "${OUTPUT0[$i]}" "${OUTPUT1[$i]}" +append /tmp/1.jpg
+    convert "${OUTPUT2[$i]}" "${OUTPUT3[$i]}" +append /tmp/2.jpg
+    convert /tmp/1.jpg /tmp/2.jpg -append "./output/result/$i.jpg"
+done
