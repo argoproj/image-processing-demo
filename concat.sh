a7f355d5b1bef0b3856261e3849c3f466e48490c
Alexander Matyushentsev <alexander@applatix.com>
2017-11-14 17:33:31 -0800

    Don't hardcode number of files in concat.sh

--- concat.sh
+++ concat.sh
@@ -7,7 +7,7 @@ OUTPUT3=($(ls -d ./output/3/????.jpg))
 
 mkdir -p ./output/result
 
-for i in {0..9}; do
+for i in $(seq 1 ${#OUTPUT0[@]}); do
     convert "${OUTPUT0[$i]}" "${OUTPUT1[$i]}" +append /tmp/1.jpg
     convert "${OUTPUT2[$i]}" "${OUTPUT3[$i]}" +append /tmp/2.jpg
     convert /tmp/1.jpg /tmp/2.jpg -append "./output/result/$i.jpg"
